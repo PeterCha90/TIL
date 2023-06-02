2312012b6513bf7b66112ab3e12a8ff36fb167b7
Peter Cha <petercha90@gmail.com>
2023-06-02 10:54:20 +0900

    update a little

--- Front/React/4.Redux/3.redux-toolkit/src/store.js
+++ Front/React/4.Redux/3.redux-toolkit/src/store.js
@@ -1,43 +1,5 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit"
 
-// const ADD = "ADD"
-// const DELETE = "DELETE"
-
-// const addToDo = (text) => {
-//   return {
-//     type: ADD,
-//     text,
-//   }
-// }
-
-// const deleteToDo = (id) => {
-//   return {
-//     type: DELETE,
-//     id: parseInt(id),
-//   }
-// }
-
-// const addToDo = createAction("ADD")
-// const deleteToDo = createAction("DELETE")
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state]
-//     case deleteToDo.type:
-//       return state.filter((toDo) => toDo.id !== action.payload)
-//     default:
-//       return state
-//   }
-// }
-
-// const reducer = createReducer([], {
-//   [addToDo]: (state, action) => {
-//     state.push({ text: action.payload, id: Date.now() })
-//   },
-//   [deleteToDo]: (state, action) => state.filter((toDo) => toDo.id !== action.payload),
-// })
-
 const toDos = createSlice({
   name: "toDosReducer",
   initialState: [],
