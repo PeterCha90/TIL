6d12d6ed073c1a2be52db81d0a2cd78b0e52867d
Peter Cha <petercha90@gmail.com>
2023-07-27 17:32:49 +0900

    carrot developed

---
+++ Front/NextJS/carrot-market/pages/chats/[id].tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next"
+
+const ChatDetail: NextPage = () => {
+  return (
+    <div className="py-10 px-4">
+      <div>
+        <div />
+        <div>Hi how much are you selling them for?</div>
+      </div>
+      <div>
+        <div />
+        <div>I want ￦20,000</div>
+      </div>
+      <div>
+        <div />
+        <div>미쳤어</div>
+      </div>
+      <div>
+        <samp></samp>
+        <div>
+          <input type="text" />
+          <div>
+            <span>&rarr;</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatDetail
